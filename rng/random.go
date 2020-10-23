61c198833d8b9072ffdf0b2fd70bf800c19a468e
Charlie_Chiu <marmot00@gmail.com>
2020-10-23 15:51:08 +0800

    refactor rng

--- rng/random.go
+++ rng/random.go
@@ -1,7 +1,6 @@
 package rng
 
 import (
-	"log"
 	"math"
 	"math/rand"
 	"time"
@@ -17,10 +16,9 @@ func New() *Rng {
 type Rng struct{}
 
 func (Rng) GenerateBust() rocket.Bust {
-	base := rand.Float64() * 1000     // 0 ~ 999.999999
-	value := math.Floor(base)/100 + 1 // 0~999 / 100 = 0 ~ 9.99 , +1 -> 1 ~ 10.99
+	base := math.Floor(rand.Float64() * 1000) // 0 ~ 999
+	value := base*0.01 + 1                    // 0~999 => 0 ~ 9.99 => 1 ~ 10.99
 	d := time.Duration(value*1000) * time.Millisecond
-	log.Println(value, d)
 	return rocket.Bust{
 		Value:    float32(value),
 		Duration: d,
